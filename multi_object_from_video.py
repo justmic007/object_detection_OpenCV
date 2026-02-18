95322565db68174f2ef23c6529b4cf065840ea1e
Micah J <justmic007@github.com>
2026-02-18 05:06:53 +0100

    multiple objects from video detection

---
+++ multi_object_from_video.py
@@ -0,0 +1,28 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO(
+    "yolov8n.pt"
+)  # Load the YOLOv8n model. You can choose other variants like 'yolov8s.pt', 'yolov8m.pt', etc. based on your needs.
+
+cap = cv2.VideoCapture("./videos/street1.mp4")  # Open the default camera (0)
+
+while True:
+    ret, frame = cap.read()  # Read a frame from the video
+
+    results = model(frame)  # Perform object detection on the frame
+
+    annotated_frame = results[0].plot()  # Get the annotated frame with detections
+
+    # Resize frame for display
+    scale_percent = 50  # Resize to 15% of original size
+    width = int(annotated_frame.shape[1] * scale_percent / 100)
+    height = int(annotated_frame.shape[0] * scale_percent / 100)
+    resized_frame = cv2.resize(annotated_frame, (width, height))
+
+    cv2.imshow("Multi Object Detection", resized_frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):  # Press 'q' to quit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
